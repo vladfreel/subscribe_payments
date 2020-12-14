b9f54fe741413bb6f401ecbb6e8440996d6a1bf8
vladfreel <vladfreelmaster@gmail.com>
2020-12-14 09:46:52 +0200

    add devise views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get "signup", to: "devise/registrations#new"
+    get "login", to: "devise/sessions#new"
+    get "logout", to: "devise/sessions#destroy"
+  end
   get '/', to: 'orders#index'
   post '/orders/submit', to: 'orders#submit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
